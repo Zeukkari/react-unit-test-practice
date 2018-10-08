a832abb808df42c675fc2a4143ef9e87267dce31
Timo Aho <zeukkari@gmail.com>
2018-10-08 06:47:52 +0300

    test practice

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from 'react'
+import { shallow } from 'enzyme'
 
-import List from './List';
+import List from './List'
 
 describe('NewToDo', () => {
   it('renders', () => {
-    const list = shallow(<List />);
+    const wrapper = shallow(<List />)
+    expect(wrapper).toBeTruthy()
+  })
 
-    expect(list).toBeTruthy();
-  });
-});
+  it('submits todo', () => {
+    const wrapper = shallow(<List />)
+    wrapper.instance().submitToDo('todo 1')
+    expect(wrapper.instance().state.toDos[0]).toEqual({
+      complete: false,
+      text: 'todo 1',
+    })
+  })
+
+  it('toggles completion', () => {
+    const wrapper = shallow(<List />)
+    wrapper.setState({
+      toDos: [
+        {
+          complete: false,
+          text: 'todo 1',
+        },
+      ],
+    })
+
+    wrapper.instance().toggleCompletion({ text: 'todo 1' })
+    expect(wrapper.instance().state.toDos[0]).toEqual({
+      complete: true,
+      text: 'todo 1',
+    })
+  })
+
+  it('doesnt toggle completion for missing text', () => {
+    const wrapper = shallow(<List />)
+    wrapper.setState({
+      toDos: [
+        {
+          complete: false,
+          text: 'todo 1',
+        },
+      ],
+    })
+
+    wrapper.instance().toggleCompletion({ text: 'todo 2' })
+    expect(wrapper.instance().state.toDos[0]).not.toEqual({
+      complete: true,
+      text: 'todo 1',
+    })
+  })
+
+  it('deletes todo', () => {
+    const wrapper = shallow(<List />)
+    wrapper.setState({
+      toDos: [
+        {
+          complete: false,
+          text: 'todo 1',
+        },
+      ],
+    })
+    wrapper.instance().deleteToDo({ text: 'todo 1' })
+    expect(wrapper.instance().state.toDos).toEqual([])
+  })
+})
--- src/components/List/NewToDo/NewToDo.test.js
+++ src/components/List/NewToDo/NewToDo.test.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from 'react'
+import { spyOn } from 'jest'
+import { shallow, mount, render } from 'enzyme'
 
-import NewToDo from './NewToDo';
+import NewToDo from './NewToDo'
 
 describe('NewToDo', () => {
   it('renders', () => {
-    const newToDo = shallow(<NewToDo submit={() => {}} />);
+    const newToDo = shallow(<NewToDo submit={() => {}} />)
+    expect(newToDo).toBeTruthy()
+  })
 
-    expect(newToDo).toBeTruthy();
-  });
-});
+  it('changes input field', () => {
+    const wrapper = mount(<NewToDo submit={() => {}} />)
+    wrapper.find('input').simulate('change', { target: { value: 'todo 1' } })
+    expect(wrapper.state('toDo')).toEqual('todo 1')
+  })
+
+  it('changes input field', () => {
+    const wrapper = mount(<NewToDo submit={() => {}} />)
+    wrapper.find('input').simulate('change', { target: { value: 'todo 1' } })
+    expect(wrapper.state('toDo')).toEqual('todo 1')
+  })
+
+  it('Submit works', done => {
+    function callback() {
+      done()
+    }
+
+    const wrapper = mount(<NewToDo submit={callback} />)
+    const spy = jest.spyOn(wrapper.instance(), 'submitTodo')
+    wrapper.find('input').simulate('change', { target: { value: 'todo 1' } })
+    const form = wrapper.find('form').simulate('submit')
+    expect(spy).toHaveBeenCalled()
+  })
+})
